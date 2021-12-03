b66848025af989744d8fe08d98abeee5594303b5
William Porter <william@porterlabs.com>
2021-12-03 09:18:38 -0800

    Day 3

--- day/3/part2.py
+++ day/3/part2.py
@@ -1,41 +1,47 @@
-gamma = ['0'] * 12
-epsilon = ['0'] * 12
-ones_count = [0] * 12
-zero_count = [0] * 12
+# Wrap all this in a function so it can be called with different inputs
+def count_bits(inputs):
+    ones_count = [0] * 12
+    zero_count = [0] * 12
+    for line in inputs:
+        for i, bit in enumerate(line):
+            if bit == '1': ones_count[i] += 1
+            else: zero_count[i] += 1
 
-with open("input") as f:
-    inputs = [input.strip() for input in f.readlines()]
+    gamma = ['0'] * 12
+    epsilon = ['0'] * 12
 
-for line in inputs:
-    for i, bit in enumerate(line):
-        if bit == '1': ones_count[i] += 1
-        else: zero_count[i] += 1
+    for i in range(12):
+        if ones_count[i] >= zero_count[i]:
+            gamma[i] = '1'
+        else:
+            epsilon[i] = '1'
 
-for i in range(12):
-    if ones_count[i] >= zero_count[i]:
-        gamma[i] = '1'
-    else:
-        epsilon[i] = '1'
+    return gamma, epsilon
 
+# Quick filter function
 def filter(f, arr):
     newarr = []
     for a in arr:
         if f(a): newarr.append(a)
     return newarr
 
-i = 0
-inp = inputs[:]
-inp2 = inputs[:]
-while len(inp) > 1 or len(inp2) > 1:
-    if len(inp) > 1:
-        inp = filter(lambda x: x[i] == gamma[i], inp)
-    if len(inp2) > 1:
-        inp2 = filter(lambda x: x[i] == epsilon[i], inp2)
+with open("input") as f:
+    inputs = [input.strip() for input in f.readlines()]
 
-    i += 1
+gamma_inp = inputs
+epsilon_inp = inputs
+for i in range(12):
+    # Important step: recount everything each time through the loop
+    gamma, _ = count_bits(gamma_inp)
+    _, epsilon = count_bits(epsilon_inp)
 
-print("gamma", ''.join(gamma))
-print("epsilon", ''.join(epsilon))
-print("inp:", inp)
-print("inp2:", inp2)
-print("total =", int(inp[0], 2) * int(inp2[0], 2))
+    if len(gamma_inp) > 1:
+        gamma_inp = filter(lambda x: x[i] == gamma[i], gamma_inp)
+    if len(epsilon_inp) > 1:
+        epsilon_inp = filter(lambda x: x[i] == epsilon[i], epsilon_inp)
+
+    if len(gamma_inp) == 1 and len(epsilon_inp) == 1:
+        break
+    
+
+print("total =", int(gamma_inp[0], 2) * int(epsilon_inp[0], 2))
