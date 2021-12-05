b2f25393dca1370720a338a70100d60621e70e2f
William Porter <william@cap-rx.com>
2021-12-05 12:07:26 -0800

    Day 5

---
+++ day/5/part2grid.py
@@ -0,0 +1,42 @@
+def add_line(diagram, line):
+    for pt in line:
+        if diagram[pt[0]][pt[1]]:
+            diagram[pt[0]][pt[1]] += 1
+        else:
+            diagram[pt[0]][pt[1]] = 1
+
+def get_line(x1, y1, x2, y2):
+    line = []
+    if x1 == x2:
+        for y in range(min(y1, y2), max(y1, y2)+1):
+            line.append((x1, y))
+    elif y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            line.append((x, y1))
+    else:
+        xfact = 1
+        yfact = 1
+        for i in range(abs(x1-x2)+1):
+            if x1 > x2:
+                xfact = -1
+            if y1 > y2:
+                yfact = -1
+            line.append((x1 + (i * xfact), y1 + (i * yfact)))
+    return line
+
+diagram = []
+for i in range(1000):
+    diagram.append([0] * 1000)
+
+with open("input") as f:
+    input = f.readlines()
+
+for pts in input:
+    pt1, pt2 = pts.split("->")
+    x1, y1 = map(int, pt1.strip().split(","))
+    x2, y2 = map(int, pt2.strip().split(","))
+    line = get_line(x1, y1, x2, y2)
+    if line:
+        add_line(diagram, line)
+
+print("total =", sum([v > 1 for r in diagram for v in r]))
